584e5eae97dc987364d88e76daa55665e1a44877
Alison Vinicius <alisonvinicius31@gmail.com>
2021-08-04 19:47:38 -0400

    Update teste.cu

--- teste.cu
+++ teste.cu
@@ -1,9 +1,14 @@
 #include<iostream>
-
+using namespace std;
 
 int main(void){
     cout << "tudo ok até aqui!" << endl;
 
     return 0;
+}
+
+__global__ void teste(){
+
+    printf("oi %d\n",threadIdx.x);
 }
 
